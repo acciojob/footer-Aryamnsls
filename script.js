927e49c32f81e3b712b71dedd0cdeadee0500624
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-04-04 21:31:20 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-// Optional: Highlight active section when clicked
-document.querySelectorAll(".box a").forEach(link => {
-    link.addEventListener("click", function() {
-        alert("You clicked: " + this.innerText);
-    });
-});
